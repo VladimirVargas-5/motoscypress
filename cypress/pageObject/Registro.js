6d4eac3c18a17a885269ecf884b6e8e7ac319409
VladimirVargas-5 <vladimir.vargas.a@gmail.com>
2021-05-12 15:43:10 -0400

    log

--- cypress/pageObject/Registro.js
+++ cypress/pageObject/Registro.js
@@ -29,6 +29,7 @@ export class registro {
         cy.get(':nth-child(3) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(ref1)
         cy.wait(2000)
         cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click()
+        cy.log('test')
 
     }
 
