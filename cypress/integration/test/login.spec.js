3f7f6d749d6382bcc5f1d89d66ee4e28d4444524
Arielcj1 <63005926+Arielcj1@users.noreply.github.com>
2021-05-13 12:41:50 -0400

    Fix logout

--- cypress/integration/test/login.spec.js
+++ cypress/integration/test/login.spec.js
@@ -12,11 +12,14 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     const regis = new registro()
     const hacer_pedido = new Hacer_pedido()
     const commons = new Commons()
-    
-    it('Crear User', ()=>{
+    beforeEach(()=>{
         commons.mainpage()
-        commons.login()
-        commons.datos('cota@motos.com','sample5')   
+        commons.datos('cota@motos.com','sample5')
+    })
+    afterEach(()=>{
+        commons.logout()
+    })
+    it('Crear User', ()=>{   
         motos.motos()
         regis.registro()
         regis.fill('testname', 'testlastname', '70512345', 'address test', 'ref test', 'https://www.google.com.bo/maps/place/Muebler%C3%ADa+Vasquez/@-16.5239053,-68.0767068,15.23z/data=!4m13!1m7!3m6!1s0x915edf0a04f5a40f:0x57dbfc76b4458ab3!2sLa+Paz!3b1!8m2!3d-16.489689!4d-68.1192936!3m4!1s0x0:0x9a1a5b77a14115c1!8m2!3d-16.5219149!4d-68.0640448?hl=es')
@@ -25,13 +28,13 @@ Cypress.on('uncaught:exception', (err, runnable) => {
         
     })
     it('Edit User', ()=>{
-        
+        motos.motos()
         regis.search('70512345')
         regis.edit('edit', 'edit', 'edit','edit')
         cy.get('.MuiSnackbar-root > .MuiPaper-root').should('be.visible')
         
     })
-    it.skip ('ir a pedidos', ()=>{
+    it.skip('ir a pedidos', ()=>{
         motos.motos()
         hacer_pedido.Hacer_pedido()
         hacer_pedido.search('70708949')
