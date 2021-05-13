d1db48ff8f5535b21f90ba439ac791a27c9019a0
Arielcj1 <63005926+Arielcj1@users.noreply.github.com>
2021-05-13 15:11:14 -0400

    Fix logout1

--- cypress/integration/test/perfilRestaurant.spec.js
+++ cypress/integration/test/perfilRestaurant.spec.js
@@ -17,28 +17,17 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     afterEach(()=>{
       commons.logout()
     })
-   /* afterEach(()=>{
-        logout.logout()
-
-    })*/
     
-    /*it ('editar perfilRestaurant', ()=>{
-       commons.clickmenu()
-       perfil.perfil_Rest()
-        perfil.editar_datos('CotaMotos')
-        })*/
-    
-   it ('agregar sucursal', ()=>{
+    it ('agregar sucursal', ()=>{
       commons.clickmenu()
       perfil.perfil_Rest()
       perfil.agregar_sucursal('prueba1' , 'Pollo' , 'Cochabamaba' , 'Centro', 'https://www.google.com.bo/maps/place/Muebler%C3%ADa+Vasquez/@-16.5239053,-68.0767068,15.23z/data=!4m13!1m7!3m6!1s0x915edf0a04f5a40f:0x57dbfc76b4458ab3!2sLa+Paz!3b1!8m2!3d-16.489689!4d-68.1192936!3m4!1s0x0:0x9a1a5b77a14115c1!8m2!3d-16.5219149!4d-68.0640448?hl=es', 'piso 3', 'Manuel' , '78596412', '1593549')
 
-})
+    })
     
-it ('editar sucursal', ()=>{
-    commons.clickmenu()
-    perfil.perfil_Rest()
-    perfil.agregar_sucursal('prueba2' , 'Pollo' , 'Cochabamaba' , 'CentroSur', 'https://www.google.com.bo/maps/place/Muebler%C3%ADa+Vasquez/@-16.5239053,-68.0767068,15.23z/data=!4m13!1m7!3m6!1s0x915edf0a04f5a40f:0x57dbfc76b4458ab3!2sLa+Paz!3b1!8m2!3d-16.489689!4d-68.1192936!3m4!1s0x0:0x9a1a5b77a14115c1!8m2!3d-16.5219149!4d-68.0640448?hl=es', 'piso 5', 'Manuela' , '78596436', '1593585')
-
+    it ('editar sucursal', ()=>{
+      commons.clickmenu()
+      perfil.perfil_Rest()
+      perfil.agregar_sucursal('prueba2' , 'Pollo' , 'Cochabamaba' , 'CentroSur', 'https://www.google.com.bo/maps/place/Muebler%C3%ADa+Vasquez/@-16.5239053,-68.0767068,15.23z/data=!4m13!1m7!3m6!1s0x915edf0a04f5a40f:0x57dbfc76b4458ab3!2sLa+Paz!3b1!8m2!3d-16.489689!4d-68.1192936!3m4!1s0x0:0x9a1a5b77a14115c1!8m2!3d-16.5219149!4d-68.0640448?hl=es', 'piso 5', 'Manuela' , '78596436', '1593585')
+    })
 })
-  })
