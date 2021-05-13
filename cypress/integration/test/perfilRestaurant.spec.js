5763f230f58aee09d993d67010cf5c2f03802320
AnaRaquel4895 <anaraquel4895@gmail.com>
2021-05-13 15:00:24 -0400

    sucursales

--- cypress/integration/test/perfilRestaurant.spec.js
+++ cypress/integration/test/perfilRestaurant.spec.js
@@ -17,7 +17,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
         commons.login()
         commons.datos('cota@motos.com','sample5')
     })
-    /*afterEach(()=>{
+   /* afterEach(()=>{
         logout.logout()
 
     })*/
@@ -25,8 +25,13 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     it ('editar perfilRestaurant', ()=>{
        commons.clickmenu()
        perfil.perfil_Rest()
-        perfil.editar_datos()
-        
-    })
+        perfil.editar_datos('CotaMotos')
+        })
+    
+   /* it ('agregar sucursal', ()=>{
+      commons.clickmenu()
+      perfil.perfil_Rest()
+      perfil.agregar_sucursal('prueba1' , 'Pollo' , 'Cochabamaba' , 'Centro', 'https://www.google.com.bo/maps/place/Muebler%C3%ADa+Vasquez/@-16.5239053,-68.0767068,15.23z/data=!4m13!1m7!3m6!1s0x915edf0a04f5a40f:0x57dbfc76b4458ab3!2sLa+Paz!3b1!8m2!3d-16.489689!4d-68.1192936!3m4!1s0x0:0x9a1a5b77a14115c1!8m2!3d-16.5219149!4d-68.0640448?hl=es', 'piso 3', 'Manuel' , '78596412', '1593549')
 
-  })
+})*/
+})
--- cypress/pageObject/perfilRestaurant.js
+++ cypress/pageObject/perfilRestaurant.js
@@ -6,9 +6,42 @@ export class perfilRestaurant {
         cy.get(':nth-child(2) > .MuiList-root > :nth-child(2) > .MuiButtonBase-root > .MuiButton-label').click()
     }
     editar_datos(value1){
-        cy.get('.jss134 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type(value1)
-        
+        cy.xpath('//*[@id="root"]/div/div/div/div/div/div/div[3]/form/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/input').clear().type(value1)
+        cy.get('input[id=logoFile]').attachFile('descarga.jpg')
+        cy.wait(2000)
+        cy.get('input[id=bannerFile]').attachFile('apple.jpg')
+        cy.wait(5000)
+        cy.get('[style="margin-top: 32px;"] > .MuiButtonBase-root').click()
+        cy.wait(5000)
+    }
+    agregar_sucursal(value1, categoria, ciudad, address,url, piso, gerente, telefono, nit ){
+        cy.get('[tabindex="-1"] > .MuiTab-wrapper > .jss178').click()
+        cy.get('.MuiButton-label').click()
+        cy.get('.jss216 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(value1)
+        cy.get('.MuiAutocomplete-popupIndicator > .MuiIconButton-label > .MuiSvgIcon-root').select(categoria)
+        cy.get('#city').select(ciudad)
+        cy.get('.jss220 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(address)
+        cy.get('.MuiInputAdornment-root > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root').type(url)
+        cy.get('.MuiDialogActions-root > .MuiButtonBase-root > .MuiButton-label').click()
+        cy.get('.MuiDialogActions-root > .MuiButtonBase-root > .MuiButton-label').type(piso)
+        cy.get('.jss222 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(gerente)
+        cy.get('.jss223 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(telefono)
+        cy.get('.jss224 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type(nit)
+        cy.get('.MuiGrid-justify-xs-center > .MuiBox-root > .MuiButtonBase-root > .MuiButton-label').click()
 
+    }
+    editar_sucursal(){
+        cy.get('[tabindex="-1"] > .MuiTab-wrapper > .jss178').click()
+        cy.get(':nth-child(1) > :nth-child(7) > .MuiBox-root > .MuiTypography-root').click()
+        cy.get('.jss260 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type(nombre)
+        cy.get('.jss265 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type(gerente)
+        cy.get('.jss266 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type (address)
+        cy.get('.jss267 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type(telefono)
+        cy.get('.jss268 > :nth-child(1) > .MuiBox-root > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type(nit)
+        cy.get('.jss270 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear().type(piso)
+        cy.get('#city').select(ciudad)
+        cy.get('.MuiAutocomplete-popupIndicator > .MuiIconButton-label > .MuiSvgIcon-root').select(categoria)
+        cy.get('.jss272 > .MuiButtonBase-root > .MuiButton-label').click()
     }
     
 }
